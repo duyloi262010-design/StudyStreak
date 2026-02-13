72cb4786838defb424cbcbda3c063251da6fb5f2
duyloi262010-design <duyloi262010@gmail.com>
2026-02-13 23:00:22 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+        base : '/StudyStreak _ Cùng bạn tiến xa/',
       server: {
         port: 3000,
         host: '0.0.0.0',
